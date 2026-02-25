0b0cb252a1c9b3acc602b36f99dd246ccbaa72bb
koishibuh <elysiagriffin@gmail.com>
2026-02-25 14:39:58 +0000

    Project setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# caylendar
-A calendar
+# yearely
+**Currently a WIP**
+
+A calendar that lets you see the entire year at a glance for easier planning
+
+
---
+++ web-app/src/data/calendar-interface.ts
@@ -0,0 +1,11 @@
+export interface ICalendar {
+    name: string;
+    dayCount: number;
+    dayStart: string;
+    weekOffset: number;
+}
+
+export interface IWeekName {
+    id: number;
+    name: string;
+}
